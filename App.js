bb487cdba37c788d4847d808158c7decfd4be178
Shubhadeep Mahato <shubhadeepmahato123@gmail.com>
2023-07-06 13:03:55 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,134 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet,Pressable, Text, TextInput, View } from 'react-native';
+import currencyData from './data.js'
+import cards from './components/Cards'
+import { useState } from 'react';
+// import Cards from './components/Cards';
 
 export default function App() {
+  const [amount, setAmount] = useState('')
+  const [currentCurrency,setCurrentCurency]=useState({})
+  const [result,setResult]=useState()
+
+  const getAmount = (text) => {
+    
+    // generateValue(currentCurrency)
+    setAmount(parseFloat(text))
+    
+  }
+  const generateValue=(targetCurrency)=>{
+    if(!amount){
+      setResult()
+      alert("Enter an amount")
+    }else{
+      setCurrentCurency(targetCurrency)
+      let value=amount* targetCurrency.value
+     setResult(value.toFixed(2))
+    }
+   
+
+   
+  
+ 
+  }
+  // const Cards = () => {
+  //   return (
+  //     <View>
+  //       <Text>Hi i am rahul</Text>
+  //     </View>
+  //   )
+  // }
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        {/* <StatusBar/> */}
+        <View>
+          <Text style={styles.heading}>Convert currency</Text>
+          <TextInput
+            style={styles.inputField}
+            placeholder='Enter Amount'
+            maxLength={12}
+            keyboardType='numeric'
+            onChangeText={getAmount}
+
+          />
+
+          {result && <Text style={styles.amount}>Amount is {result} {currentCurrency.name}</Text>}
+        </View>
+        <View style={styles.cardContainer}>
+          {
+            currencyData.map((item, index) => {
+              return (
+                <Pressable 
+                key={index} style={[styles.card,currentCurrency.name==item.name && styles.selected]}
+                onPress={()=>generateValue(item)}
+                >
+                  <Text style={styles.cardFlag}>{item.flag}</Text>
+                  <Text style={styles.cardText}>{item.name}</Text>
+                </Pressable>
+              )
+            })
+          }
+        </View>
+      </SafeAreaView>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+
+  },
+  heading: {
+    fontSize: 20,
+    fontFamily: 'serif',
+    margin: 5,
+    textAlign: 'center'
+  },
+  inputField: {
+    backgroundColor: 'gray',
+    margin: 30,
+    padding: 5,
+    borderRadius: 4
+
+  },
+  cardContainer:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+   
+  },
+  cardText:{
+    // textAlign:"center",
+    fontSize:10,
+  },
+  cardFlag:{
+    // textAlign:"center",
+    fontSize:20,
+  },
+  card:{
+    backgroundColor:'gray',
+    margin:2,
+    padding:10,
+    width:'31%',
+    borderRadius:2,
+    backgroundColor:"violet",
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  amount:{
+    textAlign:'center',
+    margin:10,
+    fontSize:15,
+    fontStyle:"italic",
+    fontWeight:"bold"
+
+  },
+  selected:{
+    backgroundColor:"green"
+  }
 });
---
+++ components/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Text, StyleSheet, View, Pressable } from 'react-native'
+
+export default function Cards (prop){
+    console.log(prop);
+        return (
+            <View>
+                <Pressable style={styles.cards}>
+                    
+                </Pressable>
+            </View>
+        )
+    
+}
+
+const styles = StyleSheet.create({
+    
+
+})
